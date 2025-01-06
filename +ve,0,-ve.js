701daaa4cb5f72b9af63d4c630bb5b2d72dbf6d0
Sana-pt <sanasadik39@gamil.com>
2025-01-06 10:43:22 +0530

    first commit

---
+++ +ve,0,-ve.js
@@ -0,0 +1,8 @@
+let a=10;
+if (a<0)
+{
+    console.log("negative number")
+}
+else{
+    console.log("positive number")
+}
---
+++ days.js
@@ -0,0 +1,25 @@
+let d=3;
+if(d===1){
+    console.log("Monday");
+}
+else if(d===2){
+    console.log("Tuesday");
+}
+else if(d===3){
+    console.log("Wednesday");
+}
+else if(d===4){
+    console.log("Thursday");
+}
+else if(d===5){
+    console.log("Friday");
+}
+else if(d===6){
+    console.log("Saturday");
+}
+else if(d===7){
+    console.log("Sunday");
+}
+else{
+    console.log("Invalid number")
+}
